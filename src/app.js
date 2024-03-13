3a2da63eee63456efcfac50f4d50b9fca22d5f24
Rosemarries <rosarinniyomsuntisuk@gmail.com>
2024-03-13 15:26:11 +0700

    Modified: complete v.4

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('pug', require('pug').__express)
 app.set('view engine', 'pug')
